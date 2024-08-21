3e4b35e4fc3b66de7f7e5d54b2128b2cdec25da9
Juan Pablo Zuluaga Mesa <juanpablozuluagamesa@gmail.com>
2024-08-21 14:21:43 -0500

    mainFrame0.1

--- mainFrame.py
+++ mainFrame.py
@@ -1,3 +1,13 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QWidget, QApplication
+import sys
+
 class ventanaPrincipal(QMainWindow):
-    pass
+    def __init__(self):
+        super().__init__()
+        self._setUI()
+
+    def _setUI(self):
+        self.setGeometry(100,100,250,250)
+        self.setWindowTitle("Aplicativo Chido")
+        self.show()
+
